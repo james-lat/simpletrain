d220a05d7654f66cc925e03ef05a7c58b716e8d7
LemonBirch <108543652+lemonbirch@users.noreply.github.com>
2024-12-23 21:12:22 -0500

    Implementing promise for pulling images before creating containers. As well as moved pull image/container to respective functions.

--- backend/dockerAPI/index.js
+++ backend/dockerAPI/index.js
@@ -2,47 +2,65 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const Docker = require('dockerode');
-
+const requirements = require('./requirements.json');
 // Configuration for connecting to the remote Docker daemon
 const docker = new Docker({ host: '192.168.40.162', port: 2375 }); 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.post('/create-container', async (req, res) => {
   const containerName = `simple-train-container-${Date.now()}`;
 
   const containerOptions = {
-    Image: 'hello-world',
+    Image: requirements.imageName,
     name: containerName,
+    Cmd: commandLineArgs
     
   };
+  console.log(requirements.imageName); 
+ pullImage(requirements.imageName)
+    .then(() => {
+        console.log(`Image ${requirements.imageName} pulled successfully on remote host.`);
+        return pullContainer(containerOptions);
+    })
+    .then((response) => { // Handle response from pullContainer
+      res.status(response.status).send(response.message);
+    })
+    .catch((error) => { // Handle errors from either function
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    });
+});
 
+async function pullImage(imageName) { 
+  const pullStream = await docker.pull(imageName);
+  return await new Promise((resolve, reject) => {
+    docker.modem.followProgress(pullStream, (err, res) => {
+        if (err) return reject(err);
+        resolve(res);
+    });
+});
+}
+
+async function pullContainer(containerOptions) { 
+  console.log("Here are the container options:", containerOptions);
   try {
-    try {
-      await docker.getImage(containerOptions.Image).inspect();
-      console.log(`Image ${containerOptions.Image} found locally.`);
-    } catch (getImageError) {
-      console.log(`Image ${containerOptions.Image} not found locally. Pulling...`);
-      await docker.pull(containerOptions.Image);
-      console.log(`Image ${containerOptions.Image} pulled successfully.`);
-    }
-
-    const existingContainers = await docker.listContainers({ all: true, filters: { name: [containerName] } });
+    const existingContainers = await docker.listContainers({ all: true, filters: { name: [containerOptions.name.toString()] } });
     if (existingContainers.length > 0) {
-      return res.status(409).send(`Container with name ${containerName} already exists.`);
+        return {message: `Container with name ${containerOptions.name.toString()} already exists.`};
     }
 
     const container = await docker.createContainer(containerOptions);
     await container.start();
-
-    res.status(201).send(`Container ${containerName} created and started with ID: ${container.id}`);
+    return { status: 201, message: `Container ${containerOptions.name} created and started with ID: ${container.id}` };
   } catch (error) {
-    console.error('Error creating container:', error);
-    res.status(500).send(`Error creating container: ${error.message}`);
+        console.error('Error creating container:', error);
+        return { status: 500, message: `Error creating container: ${error.message}` };
   }
-});
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
